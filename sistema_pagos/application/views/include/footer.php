0f4ab3495b78a991290c136e7d9d47336494db8e
aesteban <aesteban@ubuntu.ubuntu-domain>
2014-04-28 00:40:12 -0500

    Codeigniter, bootstrap y cascarón para aplicación cliente

---
+++ sistema_pagos/application/views/include/footer.php
@@ -0,0 +1,18 @@
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+<script src="//ajax.googleapis.com/ajax/libs/jqueryui/1.10.3/jquery-ui.min.js"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/lodash.js/1.3.1/lodash.min.js"></script>
+<script src="<?php echo base_url('assets/js/bootstrap.min.js') ?>"></script>
+<script src="<?php echo base_url('assets/js/custom.js') ?>"></script>
+<div id="footer">
+    <div class="inner">
+        <hr />
+        <span style="font-size: 10px">
+            Registro ante la Dirección General de Profesiones de la SEP.No. de Expediente: 01-00053, en el libro no.71-X, Secc.1ra. de Inst. Educativas foja 1215 <span style="float: right; font-weight: 600; font-size: 10px"><a style="text-decoration: none; color: #828282" href="mailto:armando.roa@upa.edu.mx">webmaster</a> - <a href="<?php echo base_url(); ?>index.php/others/mapa_sitio" style="text-decoration: none; color: #828282"/>mapa del sitio</a></span><br />
+            Calle Paseo San Gerardo No. 207. Fracc. San Gerardo. C.P. 20342. Aguascalientes, Ags.<br />
+            <span style=" font-weight: 600; font-size: 10px">Tel. Conmutador: 01 (449) 442 1400 o al 01 800 300 9370</span><br />
+        </span>
+    </div>
+</div>
+</div>
+</body>
+</html>
---
+++ sistema_pagos/application/views/include/header.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta http-equiv="Content-Type" content="text/html" charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="description" content="">
+        <meta name="keywords" content="">
+        <meta name="author" content="">
+
+        <title>UPA Sistema Pagos</title>
+
+        <link href="<?php echo base_url('assets/css/bootstrap.min.css') ?>" rel="stylesheet">
+        <link href="<?php echo base_url('assets/css/font-awesome.css') ?>" rel="stylesheet">
+        <link href="<?php echo base_url('assets/css/custom.css') ?>" rel="stylesheet">
+        <style>
+            .inner{
+                margin: 0px auto;
+                width: 960px;
+            }
+            #header{
+                background-image: url('<?php echo base_url() ?>assets/img_sist_pag/upa_header_fondo.png');
+                background-repeat: repeat-x;
+                width: 100%;
+                height: 180px;
+                margin-top: -15px;
+            }
+            #footer {
+                font-family: sans-serif;
+                color: #9FA4A5;
+                margin-top: 20px;
+                margin-bottom: 20px;
+            }
+        </style>
+    </head>
+    <body>
+        <div id="main">
+            <div id="header">
+                <div class="inner">
+                    <img src="<? echo base_url(); ?>assets/img_sist_pag/header.png" usemap="upa_header_map"/>
+                </div>
+            </div>
